9845421b5c0b33771eba8d3813dc84c5b30bc764
1 <1@DESKTOP-102BEE2>
2022-09-14 15:03:22 +0300

    Добавьте файлы проекта.

---
+++ Nuck Figgers/Program.cs
@@ -0,0 +1,17 @@
+﻿// Коваленко Артём 22-ИСП-2/2 вариант 13
+
+try
+{
+    Console.ForegroundColor = ConsoleColor.Cyan;
+    Console.WriteLine("Введите x:");
+    double x = double.Parse(Console.ReadLine());
+    Console.WriteLine("Введите y:");
+    double y = double.Parse(Console.ReadLine());
+    Console.ForegroundColor = ConsoleColor.Green;
+    double res = (((Math.Cos(x)) / (Math.PI - 2 * x)) + 16 * x * Math.Cos(x * y) - 2);
+    Console.WriteLine($"{res:F2}");
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.ToString()); 
+}
